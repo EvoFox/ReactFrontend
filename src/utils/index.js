e998cfa8445978a75d3d93986323ea0d0eb43a99
EvoFox <foulkes.joshua@live.co.uk>
2022-07-12 17:36:31 +0100

    written attempt at requests for back end, will be tested properly at a later date

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,8 +7,60 @@ export const signUp = async (username, email, pass, setter) => {
 			body: JSON.stringify({ username, email, pass }), // Body turned into JSON with stringify
 		});
 		const data = await res.json();
-		console.log(data);
 		setter(data.user.username);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const Login = async (username, pass, token, setter) => {
+	try {
+		let res = undefined;
+		if (!token) {
+			res = await fetch("http://localhost:5001/login", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					username,
+					pass,
+				}),
+			});
+		} else {
+			res = await fetch("http://localhost:5001/user", {
+				method: "GET",
+				headers: { "Content-Type": "application/json", Authorization: token },
+			});
+		}
+		const data = await res.json();
+		setter(data.user.username);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const ChangePassword = async (username, pass, newPass) => {
+	try {
+		const res = await fetch("http://localhost:5001/change-password", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ username, pass, newPass }),
+		});
+		const data = await res.json();
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const DeleteUser = async (username, email, pass) => {
+	try {
+		const res = await fetch("http://localhost:5001/delete-user", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ username, email, pass }),
+		});
+		const data = await res.json();
+		console.log(data);
 	} catch (error) {
 		console.log(error);
 	}
