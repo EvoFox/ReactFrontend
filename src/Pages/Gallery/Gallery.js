b58fc8a4b8d958fe5762bd55400dbe42746971ca
EvoFox <foulkes.joshua@live.co.uk>
2022-07-13 11:44:19 +0100

    Added Gallery page

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,9 +1,26 @@
-const Gallery = () => {
-	// Placeholder
+import { Container } from "../Shared.styled";
+import { GalleryWrapper } from "../Gallery/Gallery.styled";
+import Post from "../../components/Post/Post";
+import { Navigate } from "react-router-dom";
+
+const Gallery = ({ user, photos }) => {
 	return (
-		<>
-			<></>
-		</>
+		<Container>
+			<GalleryWrapper>
+			{!user && <Navigate to={"/"}/> }
+				{/* <Header /> display flex, row. height 150px. contains username as a drop down maybe, or perhaps a simple logout button, username can be clicked to go to profile */}
+				{photos.map((item, i) => {
+					return (
+						<Post
+						author={item.author}
+						image={item.download_url}
+						alt={`random image by author ${item.author}`}
+						key={i}
+						/>
+					);
+				})}
+			</GalleryWrapper>
+		</Container>
 	);
 };
 
---
+++ src/Pages/Gallery/Gallery.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const GalleryWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 80vw;
+    min-height: 100vh;
+`;
