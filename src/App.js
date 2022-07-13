e3021edbce5880826227b6d84d7461b642ba12c8
EvoFox <foulkes.joshua@live.co.uk>
2022-07-13 11:44:57 +0100

    removed unused variables and functions from testing, and passed photos to Gallery

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./App.css";
 import LoginForm from "./Pages/LoginForm/LoginForm";
 import Gallery from "./Pages/Gallery/Gallery";
 const App = () => {
-	const [loggedIn, setLoggedIn] = useState(false);
 	const [photos, setPhotos] = useState([]);
 	const [user, setUser] = useState();
 
@@ -18,35 +17,18 @@ const App = () => {
 
 	useEffect(() => {
 		fetchImages();
-	}, [loggedIn]);
+	}, []);
 
-	const logIn = () => {
-		// Debug function to simulate login
-		setLoggedIn((previousValue) => !previousValue);
-	};
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<LoginForm setter={setUser} user={user} />} />
-				<Route path="/gallery" element={<Gallery user={user} />} />
+				<Route
+					path="/gallery"
+					element={<Gallery user={user} photos={photos} />}
+				/>
 			</Routes>
 		</BrowserRouter>
-
-		// <Container>
-		// 	<Content>
-		// 		<Login setter={setUser} />
-		// 		<p>{user}</p>
-		// 		{!user ? (
-		// 			<h2>Not Logged In</h2>
-		// 		) : (
-		// 			photos.map((item, i) => {
-		// 				return (
-		// 					<Post author={item.author} image={item.download_url} key={i} />
-		// 				);
-		// 			})
-		// 		)}
-		// 	</Content>
-		// </Container>
 	);
 };
 
